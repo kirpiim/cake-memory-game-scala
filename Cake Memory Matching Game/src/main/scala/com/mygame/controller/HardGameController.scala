652adaaee5e15c2b58b51dd48cde662873e6039c
Your Name <your@email.com>
2025-10-04 19:54:40 +0800

    refactor: update HardGameController to JavaFX 21 style
    - Removed ScalaFXML @sfxml macro
    - Converted to standard @FXML annotations
    - Maintained GameController inheritance and handleImageClick override
    - Ensured getTotalPairs = 8 and custom endGame messages
    - Updated project to use Java 21 (was Java 8)

--- Cake Memory Matching Game/src/main/scala/com/mygame/controller/HardGameController.scala
+++ Cake Memory Matching Game/src/main/scala/com/mygame/controller/HardGameController.scala
@@ -1,24 +1,19 @@
 package com.mygame.controller
 
-import scalafxml.core.macros.sfxml
-import javafx.scene.input.MouseEvent
 import javafx.fxml.FXML
+import javafx.scene.input.MouseEvent
 
-@sfxml
 class HardGameController extends GameController {
 
   @FXML
   override def handleImageClick(event: MouseEvent): Unit = {
-    super.handleImageClick(event) // to call the gamecontroller class method to handle the click
+    super.handleImageClick(event)
   }
 
   override def getTotalPairs: Int = 8
 
   override def endGame(didWin: Boolean): Unit = {
-    if (didWin) {
-      println("Congratulations! You won the hard game!")
-    } else {
-      println("The hard game got the best of you. Try again!")
-    }
+    if (didWin) println("Congratulations! You won the hard game!")
+    else println("The hard game got the best of you. Try again!")
   }
 }
